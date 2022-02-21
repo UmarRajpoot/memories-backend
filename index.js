63a7c9dc010cd221b2658b4bc2d524c35f55eb64
umar saleem <umarraja230@gmail.com>
2022-02-21 19:38:27 +0500

    comment some code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import PostRoutes from "./routes/posts.js";
 
 const app = express();
-const PORT = process.env.PORT || 5555;
+// const PORT = process.env.PORT || 5555;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -21,9 +21,9 @@ mongoose
   })
   .then(() => {
     console.log("Connected");
-    app.listen(PORT, () => {
-      console.log("Your Server is Up and Running...");
-    });
+    // app.listen(PORT, () => {
+    //   console.log("Your Server is Up and Running...");
+    // });
   })
   .catch((error) => console.log(error.message));
 
